775e455b353478fc4ae0b965dac9755ac7f091c5
RandyRagnar <randytsiory@gmail.com>
2024-10-12 12:45:17 +0300

    resolve url

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express()
 const port = 3000
 
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'https://vue-mangas-typescript-two.onrender.com', 
   methods: ['GET', 'POST'],         
 }));
 
